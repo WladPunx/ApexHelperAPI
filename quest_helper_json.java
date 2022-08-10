7bbf78cc73692c8f124ebfa16bde3a4fdaf373f4
XaXoLOL <wlad178@mail.ru>
2022-08-10 11:27:01 +0300

    sdff

--- quest_helper_json.java
+++ quest_helper_json.java
@@ -4,7 +4,25 @@
 
 [
 
-ddasd
+
+
+{
+	"type" : "hero",
+	"name" : "Bangalore",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-bangalore.png.adapt.crop16x9.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Mad Maggie",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/01/apex-grid-tile-legends-maggie.png.adapt.crop16x9.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Bloodhunt",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-bloodhound.png.adapt.crop16x9.png"
+},
 
 
 
