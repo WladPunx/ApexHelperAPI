fc875a3587c8a4638924dbdc271ffcefe8a142f7
XaXoLOL <wlad178@mail.ru>
2022-08-11 13:43:01 +0300

    добавление всех персов

--- quest_helper_json.java
+++ quest_helper_json.java
@@ -26,6 +26,160 @@
 
 
 
+{
+	"type" : "hero",
+	"name" : "Valkyrie",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/04/apex-grid-tile-legends-valkyrie.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Wattson",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wattson.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Vantage",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/07/apex-grid-tile-legends-vantage.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Gibraltar",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-gibraltar.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Caustic",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-caustic.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Crypto",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-crypto.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Lifeline",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-lifeline.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Loba",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/05/apex-grid-tile-legends-loba.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Mirage",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-mirage.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Newcastle",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/05/apex-grid-tile-legends-newcastle.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Octane",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-octane.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Pathfinder",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-pathfinder.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Rampart",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/08/rampart/apex-grid-tile-legends-rampart.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Revenant",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/02/apex-legend-revenant-grid-tile.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Wraith",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wraith.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Seer",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/07/seer-assets/apex-grid-tile-legends-seer.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Fuse",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/01/apex-grid-tile-legends-fuse.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Horizon",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/10/horizon/apex-grid-tile-legends-horizon.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Ash",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-ash.png.adapt.crop16x9.png"
+},
+
+
+
+
+
+
+
+
+
+
+
+
+{
+	"type" : "battle_mode",
+	"name" : "Battle Royale",
+	"url_img" : ""
+}
 
 {
 	"type" : "battle_mode",
@@ -34,11 +188,7 @@
 },
 
 
-{
-	"type" : "battle_mode",
-	"name" : "Battle Royale",
-	"url_img" : ""
-}
+
 
 
 ]
