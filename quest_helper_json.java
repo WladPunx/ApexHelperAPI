e6e80689f2c8a958ba87a1143e7d5bf6d3ae2147
XaXoLOL <wlad178@mail.ru>
2022-08-12 10:57:04 +0300

    сортировка легенд

--- quest_helper_json.java
+++ quest_helper_json.java
@@ -1,53 +1,13 @@
 
 
-
-
 [
 
-
-
-{
-	"type" : "hero",
-	"name" : "Bangalore",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-bangalore.png.adapt.crop16x9.png"
-},
-
-{
-	"type" : "hero",
-	"name" : "Mad Maggie",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/01/apex-grid-tile-legends-maggie.png.adapt.crop16x9.png"
-},
 
 {
 	"type" : "hero",
 	"name" : "Bloodhunt",
 	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-bloodhound.png.adapt.crop16x9.png"
 },
-
-
-
-{
-	"type" : "hero",
-	"name" : "Valkyrie",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/04/apex-grid-tile-legends-valkyrie.png.adapt.crop16x9.png"
-},
-
-
-
-{
-	"type" : "hero",
-	"name" : "Wattson",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wattson.png.adapt.crop16x9.png"
-},
-
-
-
-{
-	"type" : "hero",
-	"name" : "Vantage",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/07/apex-grid-tile-legends-vantage.png.adapt.crop16x9.png"
-},
-
 
 
 {
@@ -57,52 +17,11 @@
 },
 
 
-{
-	"type" : "hero",
-	"name" : "Caustic",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-caustic.png.adapt.crop16x9.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Crypto",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-crypto.png.adapt.crop16x9.png"
-},
-
 
 {
 	"type" : "hero",
 	"name" : "Lifeline",
 	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-lifeline.png.adapt.crop16x9.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Loba",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/05/apex-grid-tile-legends-loba.png.adapt.crop16x9.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Mirage",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-mirage.png.adapt.crop16x9.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Newcastle",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/05/apex-grid-tile-legends-newcastle.png.adapt.crop16x9.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Octane",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-octane.png.adapt.crop16x9.png"
 },
 
 
@@ -115,8 +34,49 @@
 
 {
 	"type" : "hero",
-	"name" : "Rampart",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/08/rampart/apex-grid-tile-legends-rampart.png.adapt.crop16x9.png"
+	"name" : "Wraith",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wraith.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Bangalore",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-bangalore.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Caustic",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-caustic.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Mirage",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-mirage.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Octane",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-octane.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Wattson",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wattson.png.adapt.crop16x9.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Crypto",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-crypto.png.adapt.crop16x9.png"
 },
 
 
@@ -129,25 +89,16 @@
 
 {
 	"type" : "hero",
-	"name" : "Wraith",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-wraith.png.adapt.crop16x9.png"
+	"name" : "Loba",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/05/apex-grid-tile-legends-loba.png.adapt.crop16x9.png"
 },
 
 
 {
 	"type" : "hero",
-	"name" : "Seer",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/07/seer-assets/apex-grid-tile-legends-seer.png.adapt.crop16x9.png"
+	"name" : "Rampart",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/08/rampart/apex-grid-tile-legends-rampart.png.adapt.crop16x9.png"
 },
-
-
-
-{
-	"type" : "hero",
-	"name" : "Fuse",
-	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/01/apex-grid-tile-legends-fuse.png.adapt.crop16x9.png"
-},
-
 
 
 {
@@ -160,8 +111,50 @@
 
 {
 	"type" : "hero",
+	"name" : "Fuse",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/01/apex-grid-tile-legends-fuse.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Valkyrie",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/04/apex-grid-tile-legends-valkyrie.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Seer",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2021/07/seer-assets/apex-grid-tile-legends-seer.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
 	"name" : "Ash",
 	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/legends-character-tiles/apex-grid-tile-legends-ash.png.adapt.crop16x9.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Mad Maggie",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/01/apex-grid-tile-legends-maggie.png.adapt.crop16x9.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Newcastle",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/05/apex-grid-tile-legends-newcastle.png.adapt.crop16x9.png"
+},
+
+
+
+{
+	"type" : "hero",
+	"name" : "Vantage",
+	"url_img" : "https://media.contentapi.ea.com/content/dam/apex-legends/images/2022/07/apex-grid-tile-legends-vantage.png.adapt.crop16x9.png"
 },
 
 
@@ -173,6 +166,11 @@
 
 
 
+{
+	"type" : "battle_mode",
+	"name" : "Any Mode",
+	"url_img" : ""
+},
 
 
 {
@@ -183,12 +181,16 @@
 
 {
 	"type" : "battle_mode",
+	"name" : "NOT a Battle Royale",
+	"url_img" : ""
+},
+
+{
+	"type" : "battle_mode",
 	"name" : "Arena",
 	"url_img" : ""
 }
 
 
-
-
-
 ]
+
