5ce3f997c15a9469cdf4ce32cd18654a93d78b97
XaXoLOL <wlad178@mail.ru>
2023-01-05 13:12:39 +0300

    v3.0

--- app_update/app_update.java
+++ app_update/app_update.java
@@ -1,5 +1,5 @@
 {
-	"versionId" : 4,
-	"versionName" : "test",
-	"url": "fffff"
+	"versionId" : 3,
+	"versionName" : "3.0",
+	"url": "https://github.com/WladPunx/ApexHelperAPI/blob/master/app_update/ApexHelper_3.0_release.apk"
 }
