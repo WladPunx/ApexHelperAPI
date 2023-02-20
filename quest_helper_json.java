40a9907841c74f4f09f0917ba0c25ef8232d2c72
XaXoLOL <wlad178@mail.ru>
2023-02-20 10:36:18 +0300

    классы в API

--- quest_helper_json.java
+++ quest_helper_json.java
@@ -5,39 +5,9 @@
 
 {
 	"type" : "hero",
-	"name" : "Bloodhunt",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/bloodhunter.png"
+	"name" : "Assault",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/assault_class.png"
 },
-
-
-{
-	"type" : "hero",
-	"name" : "Gibraltar",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/gibraltor.png"
-},
-
-
-
-{
-	"type" : "hero",
-	"name" : "Lifeline",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/lifeline.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Pathfinder",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/pathinder.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Wraith",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/wraith.png"
-},
-
 
 {
 	"type" : "hero",
@@ -45,90 +15,17 @@
 	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/bangalore.png"
 },
 
-
-{
-	"type" : "hero",
-	"name" : "Caustic",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/caustic.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Mirage",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/mirage.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Octane",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/octane.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Wattson",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/wattson.png"
-},
-
-{
-	"type" : "hero",
-	"name" : "Crypto",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/crypto.png"
-},
-
-
 {
 	"type" : "hero",
 	"name" : "Revenant",
 	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/revenant.png"
 },
 
-
-{
-	"type" : "hero",
-	"name" : "Loba",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/loba.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Rampart",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/rampart.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Horizon",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/horizon.png"
-},
-
-
-
 {
 	"type" : "hero",
 	"name" : "Fuse",
 	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/fuse.png"
 },
-
-
-{
-	"type" : "hero",
-	"name" : "Valkyrie",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/valkyri.png"
-},
-
-
-{
-	"type" : "hero",
-	"name" : "Seer",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/seer.png"
-},
-
 
 {
 	"type" : "hero",
@@ -143,6 +40,128 @@
 },
 
 
+
+
+
+
+
+
+
+
+{
+	"type" : "hero",
+	"name" : "Skirmisher",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/skirmisher_class.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Pathfinder",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/pathinder.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Wraith",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/wraith.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Mirage",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/mirage.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Octane",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/octane.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Horizon",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/horizon.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Valkyrie",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/valkyri.png"
+},
+
+
+
+
+
+
+
+
+{
+	"type" : "hero",
+	"name" : "Recon",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/recon_class.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Bloodhunt",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/bloodhunter.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Crypto",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/crypto.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Seer",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/seer.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Vantage",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/vantage.png"
+},
+
+
+
+
+
+
+
+
+{
+	"type" : "hero",
+	"name" : "Support",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/support_class.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Gibraltar",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/gibraltor.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Lifeline",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/lifeline.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Loba",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/loba.png"
+},
+
 {
 	"type" : "hero",
 	"name" : "Newcastle",
@@ -151,12 +170,38 @@
 
 
 
+
+
+
+
+
+
+
+
 {
 	"type" : "hero",
-	"name" : "Vantage",
-	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/vantage.png"
+	"name" : "Controller",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/controller_class.png"
 },
 
+{
+	"type" : "hero",
+	"name" : "Caustic",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/caustic.png"
+},
+
+
+{
+	"type" : "hero",
+	"name" : "Wattson",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/wattson.png"
+},
+
+{
+	"type" : "hero",
+	"name" : "Rampart",
+	"url_img" : "https://raw.githubusercontent.com/WladPunx/ApexHelperAPI/master/rampart.png"
+},
 
 
 {
@@ -191,13 +236,8 @@
 	"type" : "battle_mode",
 	"name" : "NOT a Battle Royale",
 	"url_img" : ""
-},
-
-{
-	"type" : "battle_mode",
-	"name" : "Arena",
-	"url_img" : ""
 }
+
 
 
 ]
