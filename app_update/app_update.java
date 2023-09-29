db93c39d034b4e62810cec7fd3b55fb2b77f3921
XaXoLOL <wlad178@mail.ru>
2023-09-29 12:03:40 +0300

    static URL for download APK

--- app_update/app_update.java
+++ app_update/app_update.java
@@ -1,5 +1,5 @@
 {
 	"versionCode" : 10,
 	"versionName" : "5.2.10",
-	"url": "https://github.com/WladPunx/ApexHelperAPI/releases/tag/v.5.2.10"
+	"url": "https://github.com/WladPunx/ApexHelperAPI/releases"
 }
