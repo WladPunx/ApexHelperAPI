4d734f9de4bab40a2e34a988d10daa807b1e0640
XaXoLOL <wlad178@mail.ru>
2023-09-29 08:32:59 +0300

    v.5.2.10

--- app_update/app_update.java
+++ app_update/app_update.java
@@ -1,5 +1,5 @@
 {
-	"versionCode" : 9,
-	"versionName" : "5.1.9",
-	"url": "https://github.com/WladPunx/ApexHelperAPI/releases/tag/v.5.1.9"
+	"versionCode" : 10,
+	"versionName" : "5.2.10",
+	"url": "https://github.com/WladPunx/ApexHelperAPI/releases/tag/v.5.2.10"
 }
